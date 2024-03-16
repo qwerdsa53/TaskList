68fca5a419644abae36ae81d416c8a3f5f9b37b3
qwerdsa53 <sergeevae05@mail.ru>
2024-03-16 20:38:15 +0300

    1.1

--- app/src/main/java/com/example/tasklist/MainActivity.java
+++ app/src/main/java/com/example/tasklist/MainActivity.java
@@ -1,15 +1,48 @@
 package com.example.tasklist;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.tasklist.Adapter.ToDoAdapter;
+import com.example.tasklist.Model.ToDOModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView tasksRecycleView;
+    private ToDoAdapter tasksAdapter;
+    private List<ToDOModel> taskList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        taskList = new ArrayList<>();
+
+        tasksRecycleView = findViewById(R.id.TasksRecyclerView);
+        tasksRecycleView.setLayoutManager(new LinearLayoutManager(this));
+        tasksAdapter = new ToDoAdapter(this);
+        tasksRecycleView.setAdapter(tasksAdapter);
+
+        ToDOModel task = new ToDOModel();
+        task.setTask("This is a task");
+        task.setStatus(0);
+        task.setId(1);
+
+        taskList.add(task);
+        taskList.add(task);
+//        taskList.add(task);
+//        taskList.add(task);
+//        taskList.add(task);
+
+
+
+        tasksAdapter.setTasks(taskList);
 
     }
 }
